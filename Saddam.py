c27ac81e2961661917fe6f19cd9443b67b5f7a95
anonsecbotnetstudent19 <155124528+anonsecbotnetstudent19@users.noreply.github.com>
2024-02-05 13:38:14 +0700

    Update Saddam.py

--- Saddam.py
+++ Saddam.py
@@ -281,7 +281,7 @@ class DDoS(object):
 def main():
 	parser = OptionParser(usage=USAGE)
 	for args, kwargs in OPTIONS:
-		parser.add_option(*args, **kwargs)
+		parser.add_option(args=args, **kwargs)
 	options, args = parser.parse_args()
 	domains = None
 	if len(args)<1:
